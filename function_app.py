e718e22b032bca7fa33bc63ca71e3d022afdf5c8
sweeeetv <xgao566@aucklanduni.ac.nz>
2026-03-04 13:29:26 +0800

    testing terraform crc -1

--- function_app.py
+++ function_app.py
@@ -6,14 +6,12 @@ connection_string = os.environ["COSMOS_DB_CONNECTION_STRING"]
     
     # 1. Connect using the Table API Specialist
     # This automatically looks for 'TableEndpoint' in your string!
-table_client = TableClient.from_connection_string(conn_str=connection_string, table_name="user-counter")
+table_client = TableClient.from_connection_string(conn_str=connection_string, table_name="visiter-counter-table")
 app = func.FunctionApp()
 
 @app.route(route="GetResumeCounter", auth_level=func.AuthLevel.ANONYMOUS)
 def GetResumeCounter(req: func.HttpRequest) -> func.HttpResponse:
     
-    
-
     # 2. Get the specific row (PartitionKey and RowKey are the 'Table' way)
     # We assume you have a row where PartitionKey="visitor_stats" and RowKey="1"
     entity = table_client.get_entity(partition_key="visitor_stats", row_key="1")
